b35fe5f66163ffab76683dab1cbb9f01ff3cddb0
personal <krishnendu1103.be21@chitkarauniversity.edu.in>
2024-08-14 09:39:57 +0530

    done project

---
+++ Encapsulation.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class Encapsulation {
+//    class student_1{
+//        String name;
+//        int roll_no;
+//        int age;
+//        int marks;
+//        String Address;
+//
+//        student_1(){
+//            name="krishnendu";
+//            roll_no=
+//        }
+
+    public static void main(String[] args) {
+
+    }
+}
